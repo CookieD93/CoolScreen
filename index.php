ed61c4c1a5cedd1547b48c5a418b2346ecebb70e
CookieD93 <dani9035@edu.easj.dk>
2017-11-28 13:48:31 +0100

    Refactored to use Twig Setup

--- index.php
+++ index.php
@@ -1,37 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Index</title>
-</head>
-<body>
  <?php
- date_default_timezone_set("Europe/Copenhagen");
- echo date("H:i");
 
 
-/*$BaseUri ="http://www.yr.no/place/Denmark/Zealand/Roskilde/forecast_hour_by_hour.xml";
-$ForeCast = ""
-
-
-$uri= $BaseUri;
-$xmlData = file_get_contents($uri);
-$convertToAssociativeArray = true;
-$ForeCast =
-*/
 
 $ForeCast = simplexml_load_file("http://www.yr.no/place/Denmark/Zealand/Roskilde/forecast_hour_by_hour.xml");
-echo "<pre>";
 //var_dump ($ForeCast->forecast->tabular->time);
 //$NextForeCast = $ForeCast->forecast->tabular->time;
 $NextForeCast = $ForeCast->forecast->tabular->time;
 
-echo $NextForeCast[0]->symbol->attributes()["name"]."</br>";
-echo $NextForeCast[0]->temperature->attributes()["value"]."&deg; ";
-echo $NextForeCast[0]->temperature->attributes()["unit"];
- echo "</pre>";
+$WeatherSymbolName = $NextForeCast[0]->symbol->attributes()["name"];
+$WeatherTemperature = $NextForeCast[0]->temperature->attributes()["value"];
+$TemperatureUnit = $NextForeCast[0]->temperature->attributes()["unit"];
+ date_default_timezone_set("Europe/Copenhagen");
+ $date = date("H:i");
 
- ?>
-    <img src="cam.jpg" width="400">
-</body>
-</html>
+
+ require_once 'vendor/autoload.php';
+ Twig_Autoloader::register();
+ $loader = new Twig_Loader_Filesystem('views');
+ $twig = new Twig_Environment($loader, array(
+     'auto_reload' => true
+ ));
+ $template = $twig->loadTemplate('index.html.twig');
+ $parametersToTwig = array("Date"=>$date,"WeatherSymbolName" => $WeatherSymbolName,"WeatherTemperatue" => $WeatherTemperature, "TemperatureUnit" =>$TemperatureUnit);
+ echo $template->render($parametersToTwig);
+
