a426cc08a734d579bbbc13d3f3fba005e7560ae9
CookieD93 <dani9035@edu.easj.dk>
2017-12-04 15:09:27 +0100

    Added "noter" files and navigation to and from

--- controllers/noter.php
+++ controllers/noter.php
@@ -1,2 +1,11 @@
 <?php
 
+require_once '../vendor/autoload.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('../views');
+$twig = new Twig_Environment($loader, array(
+    'auto_reload' => true
+));
+$template = $twig->loadTemplate('LæsOpskrift.html.twig');
+$parametersToTwig = array();
+echo $template->render($parametersToTwig);
--- views/Nav.php
+++ views/Nav.php
@@ -2,5 +2,5 @@
     <a href="../controllers/index.php">Forside</a>
 
     <a href="../controllers/Opskrifter.php">Opskrifter</a>
-    <a href="../controllers/">Noter</a>
+    <a href="../controllers/noter.php">Noter</a>
 </nav>
